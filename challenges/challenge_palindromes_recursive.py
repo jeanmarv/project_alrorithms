57f34ddb56b4be42cae6267145d42fa026f4caa7
Jean Marcel <jeanvanzella91@gmail.com>
2022-09-26 01:07:15 -0300

    stydu challenge and recursive palindromes

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
+    word_len = word[low_index:high_index]
+    
+    if word == "":
+        return False
+
+    if len(word_len) < 1:
+        return True
+
+    if word[low_index] != word[high_index]:
+        return False
+
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    counter = 0
+    try:
+        for key, value in permanence_period:
+            if target_time >= key and int(target_time) <= value:
+                counter += 1
+        return counter
+    except TypeError:
+        return None
