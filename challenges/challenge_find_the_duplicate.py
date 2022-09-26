8d8b99e93317fd1f36b54d7de0adee7242420238
Jean Marcel <jeanvanzella91@gmail.com>
2022-09-26 15:58:00 -0300

    fixing complexity

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,11 +1,24 @@
+def negative_str(nums):
+    for itens in nums:
+        if isinstance(itens, str) or itens < 0 or itens == "":
+            return False
+
+
 def find_duplicate(nums):
     sorted_nums = sorted(nums)
+    if negative_str(sorted_nums) is False:
+        return False
+    if (return_duplicate(sorted_nums) is not False
+       and return_duplicate(sorted_nums) is not None):
+        return return_duplicate(sorted_nums)
+    else:
+        return False
+
+
+def return_duplicate(nums):
     try:
-        for i, itens in enumerate(sorted_nums):
-            if isinstance(itens, str) or itens < 0:
-                return False
-            if itens == sorted_nums[i+1]:
+        for i, itens in enumerate(nums):
+            if itens == nums[i+1]:
                 return itens
     except IndexError:
         return False
-    return False
