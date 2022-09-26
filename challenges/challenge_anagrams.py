942f64cbf358dfba31cd3b588c1c46ca960fbfea
Jean Marcel <jeanvanzella91@gmail.com>
2022-09-26 12:08:19 -0300

    build a sort type function, req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,14 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
+    first_list = list(first_string.lower())
+    second_list = list(second_string.lower())
+
+    for itens in first_list:
+        if itens in second_list:
+            second_list.remove(itens)
+        else:
+            return False
+    if len(second_list) == 0:
+        return True
+    else: return False
+
+print(is_anagram("pedra", "perdaa"))
